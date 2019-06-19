ed01e056fc963a2413e9819565d1f9f6cdf70ccc
MarcinBalejko <mbbm9@o2.pl>
2019-06-19 23:46:52 +0200

    :white_check_mark: Post method

--- app.rb
+++ app.rb
@@ -13,9 +13,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb :cat_form
+end
+
+post '/named-cat' do
   p params
   @name = params[:name]
   erb :index
 end
-

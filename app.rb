b690661a07980dde113abe80087002fc9d38def4
MarcinBalejko <mbbm9@o2.pl>
2019-06-19 17:02:30 +0200

    :white_check_mark: /named-cat params

--- app.rb
+++ app.rb
@@ -8,7 +8,12 @@ get '/secret' do
     'This is a secret page'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
+  erb :index
 end
